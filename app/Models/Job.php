143648de3bde0f77ea3f92e8bb83d4dc4ae2b690
Simon Wong <swongkc13@gmail.com>
2026-01-17 16:50:16 +0100

    Completed Day 12: Pivot Tables and BelongsToMany Relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,8 +11,14 @@ class Job extends Model
 
     protected $fillable = ['title', 'salary'];
 
-    public function employer(){
+    public function employer()
+    {
         return $this->belongsTo(Employer::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
     }
 }
 ;
