8433fdb8b2ba8035b82b8fe96349ad23fb54a8fc
Simon Wong <swongkc13@gmail.com>
2026-01-19 14:33:21 +0100

    Day 16: Forms and CSRF Explained

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,7 +9,7 @@ class Job extends Model
     use HasFactory;
     protected $table = 'job_listings';
 
-    protected $fillable = ['title', 'salary'];
+    protected $fillable = ['employer_id', 'title', 'salary'];
 
     public function employer()
     {
