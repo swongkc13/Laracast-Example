5404a21e5da63ad9cea55e436f7e0b76a4a55092
Simon Wong <swongkc13@gmail.com>
2026-01-21 10:54:06 +0100

    Day 20: Starter Kits & Day 21: Login & Registration Forms

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -9,58 +9,31 @@
             <div class="border-b border-gray-900/10 pb-12">
                 <h2 class="text-base/7 font-semibold text-gray-900">Create a New Job</h2>
                 <p class="mt-1 text-sm/6 text-gray-600">We just need a handful of details from you.</p>
-
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                    <x-form-field>
+                        <x-form-label for="title">Title</x-form-label>
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                            <x-form-input id="title" name="title" placeholder="CEO" />
 
-                                <input id="title" type="text" name="title" placeholder="Shift Leader"
-                                    class="block min-w-0 grow bg-white py-1.5 px-2 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    required />
-                            </div>
-
-                            @error('title')
-                                <p class="text-xs text-red-500 font-semibold mt-1"> {{ $message }}</p>
-                            @enderror
+                            <x-form-error name="title" />
                         </div>
-                    </div>
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for="salary">Salary</x-form-label>
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                            <x-form-input id="salary" name="salary" placeholder="$50,000" />
 
-                                <input id="salary" type="text" name="salary" placeholder="$50,000 Per Year"
-                                    class="block min-w-0 grow bg-white py-1.5 px-2 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    required />
-                            </div>
-                            @error('salary')
-                                <p class="text-xs text-red-500 font-semibold mt-1"> {{ $message }}</p>
-                            @enderror
+                            <x-form-error name="salary" />
                         </div>
-
-                        <!-- <div class="mt-10">
-                            @if($errors->any())
-                                <ul>
-                                    @foreach($errors->all() as $error)
-                                        <li class="text-red-500 italic">{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            @endif
-                        </div> -->
-                    </div>
-
+                    </x-form-field>
 
 
                 </div>
 
                 <div class="mt-6 flex items-center justify-end gap-x-6">
                     <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-                    <button type="submit"
-                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                    <x-form-button>Save</x-form-button>
                 </div>
     </form>
 
