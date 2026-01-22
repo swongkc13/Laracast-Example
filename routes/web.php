80cb674ce11150eb35d08155815718119b7751fb
swongkc13 <swongkc13@gmail.com>
2026-01-22 15:31:01 +0100

    Day 24: Preview and Send Emails Using Mailable Classes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
-
-
 
 
 Route::view('/', 'home');
